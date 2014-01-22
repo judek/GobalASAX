fad6c8723896c25a0d2266f16c860308a0f7b469
jude <judek15@gmail.com>
2014-01-22 07:41:45 -0600

    Initial Commit

---
+++ GobalASAX/Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GobalASAX
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            if (IsPostBack)
+                return;
+
+            DisplayTweets();
+
+            //Session["Tweets"] = new List<string> {"Tweet1", "Tweet2"};
+            
+        }
+
+        protected void ButtonAddTweet_Click(object sender, EventArgs e)
+        {
+          
+
+            AddTweet(TextBoxTweet.Text);
+            
+            TextBoxTweet.Text = "";
+
+           
+            
+            DisplayTweets();
+
+
+
+        }
+
+        
+        protected void ButtonEndSession_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+        }
+
+        void DisplayTweets()
+        {
+            if (null == Session["Tweets"])
+                return;
+
+            List<string> tweets = Session["Tweets"] as List<string>;
+
+
+            string htmlTweets = "";
+            foreach (string tweet in tweets)
+            {
+                
+                if (tweet.Length > 0)
+                    htmlTweets += ("<br>" + tweet);
+            }
+
+            LiteralTweets.Text = htmlTweets;
+        }
+
+        protected void AddTweet(string tweet)
+        {
+
+
+            if (null == Session["Tweets"])
+                Session["Tweets"] = new List<string>();
+
+            List<string> tweets = Session["Tweets"] as List<string>;
+
+            tweets.Add(tweet);
+
+        }
+      
+    }
+}
---
+++ GobalASAX/Global.asax.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Net.Mail;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System.IO;
+using System.Timers;
+
+namespace GobalASAX
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        //System.Timers.Timer _timer = null;
+        
+        public static void SendMailMessage(MailMessage mail)
+        {
+            //set the addresses
+            if (null == mail.From)
+                mail.From = new MailAddress("\"River Valley Community Church\" <2bc17e6f292e@rivervalleycommunity.org>");
+            //mail.From = new MailAddress(sFrom);
+
+            //send the message
+            SmtpClient smtp = new SmtpClient("mail.rivervalleycommunity.org");
+
+            System.Net.NetworkCredential SmtpUser;
+            SmtpUser = new System.Net.NetworkCredential();
+            SmtpUser.UserName = "2bc17e6f292e@rivervalleycommunity.org";
+            SmtpUser.Password = "Myza2828";
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = SmtpUser;
+
+           
+            smtp.Send(mail);
+
+        }
+
+        public static void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+
+        }
+
+
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //_timer = new System.Timers.Timer(5*60*1000);//Every 5 Minutes
+            //_timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+            //_timer.Enabled = true;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+            //string SessionGUID = "";
+            //try
+            //{
+                
+            //    SessionGUID =  Guid.NewGuid().ToString();
+            //    Session["GUID"] = SessionGUID;
+                string sPath = Server.MapPath("Global.asax.Trace.txt");
+            //    File.AppendAllText(sPath, "\r\n" + DateTime.Now + " Session " + SessionGUID + " Started");
+                Session["TraceFilePath"] = sPath;
+            //}
+            //catch 
+            //{ 
+            
+            //}
+            
+         
+         
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+         
+            List<string> SessionTweets = Session["Tweets"] as List<string>;
+
+            if (null == SessionTweets)
+                return;
+            
+            string sPath = Session["TraceFilePath"] as string;
+            //string SessionGUID = "UNKNOWN";
+            //string temp = Session["GUID"] as string;
+            //if (temp != null)
+            //    SessionGUID = temp.Clone() as string;
+           
+          
+            //try
+            //{
+            //    if (null != sPath)
+            //        File.AppendAllText(sPath, "\r\n"+ DateTime.Now.ToLongTimeString() + " Session " + SessionGUID + " Ended");
+            //}
+            //catch { }
+
+          
+            foreach (string tweet in SessionTweets)
+            {
+                #region SendTweet
+
+                using (MailMessage tweetMessage = new MailMessage())
+                {
+                    try
+                    {
+                        tweetMessage.Body = tweet;
+                        tweetMessage.To.Add("tweet@tweetymail.com");
+                        tweetMessage.To.Add("judek@yahoo.com");
+                        SendMailMessage(tweetMessage);
+                    }
+                    catch (Exception exp)
+                    {
+                        try
+                        {
+                            if (null != sPath)
+                            {
+                                File.AppendAllText(sPath, "\r\n Send Mail Error:" + DateTime.Now + exp.Message + "\r\n" + exp.StackTrace);
+                                File.AppendAllText(sPath, "\r\n Message Body:\r\n" + tweetMessage);
+                                File.AppendAllText(sPath, "\r\n Abborting...");
+                            }
+                            break;//If one mail breaks the all probably will so stop trying
+                        }
+                        catch {break; }
+                    }
+       
+                }
+
+                #endregion
+            }
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            //_timer.Enabled = false;
+            //_timer.Dispose();
+
+        }
+    }
+}
